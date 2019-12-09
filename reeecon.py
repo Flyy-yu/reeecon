5e3779e477b499540337f673b92d7fc0b844c900
Flyy-yu <peiyu94@gatech.edu>
2019-12-09 00:06:48 -0500

    add directory

--- reeecon.py
+++ reeecon.py
@@ -1,5 +1,6 @@
 import pathlib
-from .module.subdomain import *
+import os
+from module.subdomain import *
 from datetime import date
 
 today = date.today().strftime("%b-%d-%Y")
@@ -8,13 +9,16 @@ if __name__ == '__main__':
     target = 'certik'
 
     # TODO better path
-    out_dir = '/root/recon_result/{}/{}/'.format(today, target)
+    out_dir = '/root/recon_result/{}/{}/'.format(target, today)
 
     print("subdomain recon:")
     subdomain_path = out_dir + 'subdomain/'
-    pathlib.Path(subdomain_path).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(subdomain_path).mkdir(parents=True)
 
     use_amass('certik.org', subdomain_path)
     use_subfinder('certik.org', subdomain_path)
 
-    run_tool('cat {} | --sort --unique > ../subdomain.txt'.format(subdomain_path))
+    os.system('cat {}* | sort --unique > {}subdomain.txt'.format(subdomain_path, out_dir))
+    os.system('cat {}subdomain.txt | httprobe -c 50 -t 3000 > {}responsive.txt'.format(out_dir, out_dir))
+
+    
