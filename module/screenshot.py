e3bbcc99ec0045866ab1bc1822e95b392bfc9dac
fly <fly@flys-MacBook-Pro.local>
2020-06-24 14:47:44 -0400

    fix aquatone out path

--- module/screenshot.py
+++ module/screenshot.py
@@ -6,5 +6,5 @@ def use_aquatone(out_dir):
     responsive_url_list = out_dir + 'responsive.txt'
 
     os.system('cat {} | aquatone -out {} -threads 5 -silent'
-              .format(responsive_url_list, home, out_dir))
+              .format(responsive_url_list, out_dir))
     return
