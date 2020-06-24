37342b4a6592a8f641432e0d03b028d79dbc1753
fly <fly@flys-MacBook-Pro.local>
2020-06-24 02:11:38 -0400

    test

--- module/directory.py
+++ module/directory.py
@@ -8,7 +8,7 @@ def use_dirsearch_full_url(url, wordlist, out_dir):
     filename = get_directory_filename(url) + '_dirsearch.txt'
     out_dir = out_dir + filename
     run_tool('python3 {} -u {} -b -w {} -t 50 -E --plain-text-report {}'
-             .format(dirs_path, url, wordlist, out_dir))
+             .format(dirs_path, url, home + wordlist, out_dir))
     return
 
 
@@ -16,7 +16,7 @@ def use_dirsearch_short_url(url, wordlist, out_dir):
     filename = get_directory_filename(url) + '_dirsearch.txt'
     out_dir = out_dir + filename
     run_tool('python3 {} -u {} -b -w {} -t 50 -e {} --plain-text-report {}'
-             .format(dirs_path, url, wordlist, f_ext, out_dir))
+             .format(dirs_path, url, home + wordlist, f_ext, out_dir))
     return
 
 
@@ -24,5 +24,5 @@ def use_gobuster(url, wordlist, out_dir):
     filename = get_directory_filename(url) + '_gobuster.txt'
     out_dir = out_dir + filename
     run_tool('gobuster dir -u {} -w {} -o {} -t 20 --wildcard'
-             .format(url, wordlist, out_dir))
+             .format(url, home + wordlist, out_dir))
     return
--- module/screenshot.py
+++ module/screenshot.py
@@ -5,6 +5,6 @@ from .util import *
 def use_aquatone(out_dir):
     responsive_url_list = out_dir + 'responsive.txt'
 
-    os.system('cat {} | aquatone -chrome-path {}/chromium-latest-linux/latest/chrome -out {} -threads 5 -silent'
+    os.system('cat {} | aquatone -out {} -threads 5 -silent'
               .format(responsive_url_list, home, out_dir))
     return
